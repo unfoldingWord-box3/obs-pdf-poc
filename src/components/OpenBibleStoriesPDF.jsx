2ad74c6ddbb6d47cb302007df8dca948938585f8
klappy <christopher@klapp.name>
2021-11-03 15:29:10 -0400

    usePrintPreview custom hook

--- src/components/OpenBibleStoriesPDF.jsx
+++ src/components/OpenBibleStoriesPDF.jsx
@@ -1,47 +1,15 @@
-import {useLayoutEffect, useCallback} from 'react';
-import { Previewer } from 'pagedjs';
+import React from 'react';
+import usePrintPreview from '../hooks/usePrintPreview';
 
 import OpenBibleStories from './OpenBibleStories';
 
 export default function OpenBibleStoriesPDF (props) {
-
   const { embed } = props;
+
+  const component = <OpenBibleStories {...props} />
+  const printPreviewComponent = usePrintPreview({ component, embed });
   
-  const previewPDF = useCallback(() => {
-      const previewer = new Previewer();
-
-      const callback = (flow) => {
-        console.log("preview rendered, total pages", flow.total, { flow });
-      };
-
-      let options;
-      if (embed) {
-        previewer.preview(
-          document.querySelector('#content').innerHTML,
-          [],
-          document.querySelector('#preview')
-        ).then(flow => {
-          callback(flow);
-          document.querySelector('#content').style.display = 'none';
-        });
-      } else {
-        previewer.preview(options).then(callback);
-      };
-    }, [embed]);
-
-  useLayoutEffect(() => {
-    previewPDF();
-  }, [previewPDF, embed]);
-  
-  return (
-    <>
-      <div id="content">
-        <OpenBibleStories {...props} />
-      </div>
-      <div id="preview"></div>
-    </>
-  )
-
+  return printPreviewComponent;
 };
 
 
